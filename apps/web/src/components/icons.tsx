0754050bd643ec26170c538599acf4d19ffb315b
Dante Vicenzo <dantevcz@gmail.com>
2026-06-19 02:44:12 -0300

    fix: change ProfileIcon fill to emoji-like gray (#666)

--- apps/web/src/components/icons.tsx
+++ apps/web/src/components/icons.tsx
@@ -249,16 +249,16 @@ export function ProfileIcon({ className }: IconProps) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect x="6" y="1" width="4" height="1" fill="#000"/>
-      <rect x="5" y="2" width="6" height="1" fill="#000"/>
-      <rect x="4" y="3" width="8" height="3" fill="#000"/>
-      <rect x="5" y="6" width="6" height="1" fill="#000"/>
-      <rect x="6" y="7" width="4" height="1" fill="#000"/>
-      <rect x="4" y="9" width="8" height="1" fill="#000"/>
-      <rect x="3" y="10" width="10" height="1" fill="#000"/>
-      <rect x="2" y="11" width="12" height="2" fill="#000"/>
-      <rect x="3" y="13" width="10" height="1" fill="#000"/>
-      <rect x="4" y="14" width="8" height="1" fill="#000"/>
+      <rect x="6" y="1" width="4" height="1" fill="#666"/>
+      <rect x="5" y="2" width="6" height="1" fill="#666"/>
+      <rect x="4" y="3" width="8" height="3" fill="#666"/>
+      <rect x="5" y="6" width="6" height="1" fill="#666"/>
+      <rect x="6" y="7" width="4" height="1" fill="#666"/>
+      <rect x="4" y="9" width="8" height="1" fill="#666"/>
+      <rect x="3" y="10" width="10" height="1" fill="#666"/>
+      <rect x="2" y="11" width="12" height="2" fill="#666"/>
+      <rect x="3" y="13" width="10" height="1" fill="#666"/>
+      <rect x="4" y="14" width="8" height="1" fill="#666"/>
     </svg>
   )
 }
