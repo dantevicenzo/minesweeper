19918aead4dc8d7c54da5b379bb27f269881908b
Dante Vicenzo <dantevcz@gmail.com>
2026-06-19 03:12:24 -0300

    fix: change profile icon color to #517598

--- apps/web/src/components/icons.tsx
+++ apps/web/src/components/icons.tsx
@@ -75,7 +75,7 @@ export function TimerDigit({ digit, className }: { digit: number } & IconProps) 
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path fill="red" d={data.outline} transform={data.transform} />
+      <path fill="#cc0000" d={data.outline} transform={data.transform} />
       {data.inner && <path fill="maroon" fillOpacity="0.3" d={data.inner} transform={data.transform} />}
     </svg>
   )
@@ -326,16 +326,16 @@ export function ProfileIcon({ className }: IconProps) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect x="6" y="1" width="4" height="1" fill="#4a5a6d"/>
-      <rect x="5" y="2" width="6" height="1" fill="#4a5a6d"/>
-      <rect x="4" y="3" width="8" height="3" fill="#4a5a6d"/>
-      <rect x="5" y="6" width="6" height="1" fill="#4a5a6d"/>
-      <rect x="6" y="7" width="4" height="1" fill="#4a5a6d"/>
-      <rect x="4" y="9" width="8" height="1" fill="#4a5a6d"/>
-      <rect x="3" y="10" width="10" height="1" fill="#4a5a6d"/>
-      <rect x="2" y="11" width="12" height="2" fill="#4a5a6d"/>
-      <rect x="3" y="13" width="10" height="1" fill="#4a5a6d"/>
-      <rect x="4" y="14" width="8" height="1" fill="#4a5a6d"/>
+      <rect x="6" y="1" width="4" height="1" fill="#517598"/>
+      <rect x="5" y="2" width="6" height="1" fill="#517598"/>
+      <rect x="4" y="3" width="8" height="3" fill="#517598"/>
+      <rect x="5" y="6" width="6" height="1" fill="#517598"/>
+      <rect x="6" y="7" width="4" height="1" fill="#517598"/>
+      <rect x="4" y="9" width="8" height="1" fill="#517598"/>
+      <rect x="3" y="10" width="10" height="1" fill="#517598"/>
+      <rect x="2" y="11" width="12" height="2" fill="#517598"/>
+      <rect x="3" y="13" width="10" height="1" fill="#517598"/>
+      <rect x="4" y="14" width="8" height="1" fill="#517598"/>
     </svg>
   )
 }
